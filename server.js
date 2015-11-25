c30a90055e2c6c7113bea660e07de7abd4e7682c
doemsche <doemsche@gmx.ch>
2015-11-25 17:25:14 +0100

    adjust for remote usage

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var Webdino = require('./lib/Webdino');
 var app = new Webdino();
 
 var PORT = 5000;
+//retrieve IPAddress
 
 app.on("ready", function() {
 	this.httpserver.listen(PORT, function() {
-		console.log("Listening on :"+PORT+" waiting for API to be consumed")
+		console.log("Listening on  => "+PORT+" waiting for API to be consumed")
 	});
-})
+});
