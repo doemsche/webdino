6a990def6989356257b4afa75ce7ff466abf0485
doemsche <doemsche@gmx.ch>
2015-11-25 16:27:51 +0100

    curcial update to cunstructor function pattern

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-var webuino = require('./lib/Webdino');
-var webdinoApp = webuino();
+
+var Webdino = require('./lib/Webdino');
+var app = new Webdino();
 
 var PORT = 5000;
 
-// function getAllProperties(obj){
-//     var allProps = []
-//       , curr = obj
-//     do{
-//         var props = Object.getOwnPropertyNames(curr)
-//         props.forEach(function(prop){
-//             if (allProps.indexOf(prop) === -1)
-//                 allProps.push(prop)
-//         })
-//     }while(curr = Object.getPrototypeOf(curr))
-//     return allProps
-// }
- 
-webdinoApp.on("ready", function() {
-  this.httpserver.listen(PORT, function() {
-    console.log("Listening on :"+PORT+" waiting for API to be consumed")
-  });
+app.on("ready", function() {
+	this.httpserver.listen(PORT, function() {
+		console.log("Listening on :"+PORT+" waiting for API to be consumed")
+	});
 })
